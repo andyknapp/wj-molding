7d0fbe9b221302c67ef8aa01192259092be40c4d
Andy <aknapp1@gmail.com>
2020-06-02 17:41:06 -0400

    sub page styles

--- wp-content/themes/wj/page.php
+++ wp-content/themes/wj/page.php
@@ -21,13 +21,12 @@ get_header();
     </div>
 </section>
 
-<section class="text-block gray-cone-b wide">
+<section class="text-block plain wide sub-page">
     <div class="text-block-content">
-        <h1 class="heading-2">Precision </br>Injection Molding</h1>
+        <!-- <h1 class="heading-2">Precision </br>Injection Molding</h1> -->
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/wj-molding-widget.jpg" alt="" class="alignright">
         <p class="copy-2">As an industry leader in custom injection molding for over 30 years, we take pride in our reputation for delivering high-quality, tight-tolerance, injection molded products.</p>
         <p class="copy-2">Our veteran engineers and manufactures bring experience with both engineering grade and commodity resins to meet your production needs and desired outcomes.</p>
-
         <aside class="sub-section">
             <h2 class="heading-3">Our Core Competencies</h2>
             <ul>
@@ -41,51 +40,18 @@ get_header();
             </ul>
         </aside>
 
+        <h3 class="heading-1">Tooling</h3>
+        <p class="copy-1">Our in-house tooling capabilities and relationships with local suppliers means we’re able to meet the needs of any injection molding project. From quote to production, our customer-centric approach ensures on-time delivery of your tooling requirements. Our dedicated engineers provide full project management to ensure the optimal launch of your tooling.</p>
+        <p class="copy-1">In addition to new builds, our system successfully launches transfer tools with minimal contingencies. Please contact us so we can provide a seamless transition.</p>
+
+        <h3 class="heading-1">Equipment</h3>
+        <p class="copy-1">WJ Molding currently offers tonnage ranging from 80 to 250 horizontal and 80-ton vertical rotary. All machines have dedicated equipment, including grinders, dryers, and sprue pickers. </p>
     </div>
 </section>
 
 <?php //include('template-parts/cta-block.php'); ?>
 
-<?php include('template-parts/fixed-bg-button-block.php'); ?>
-
-<section class="intro">
-    <div class="container-small">
-        <h2 class="heading-1">Innovation in Injection Molding</h2>
-        <p class="copy-1">With over 30 years of experience in custom injection molding, WJ Molding, Inc. is your source for high-quality, complex manufactured injection molded components. Our team combines expert advice in engineering and molding with innovative solutions, and the highest level of support to exceed your expectations, every time.</p>
-    </div>
-
-    <aside class="industries">
-        <ul class="container-med">
-            <li>
-                <svg class="silo">
-                    <use xlink:href="#icon-silo"></use>
-                </svg>
-                <span class="heading-4">Agriculture</span>
-            </li>
-            <li>
-                <svg class="car">
-                    <use xlink:href="#icon-car"></use>
-                </svg>
-                <span class="heading-4">Automotive</span>
-            </li>
-            <li>
-                <svg class="television">
-                    <use xlink:href="#icon-television"></use>
-                </svg>
-                <span class="heading-4">Consumer</span>
-            </li>
-            <li>
-                <svg class="medical">
-                    <use xlink:href="#icon-medical"></use>
-                </svg>
-                <span class="heading-4">Medical</span>
-            </li>
-        </ul>
-    </aside>
-</section>
-
-
-
+<?php //include('template-parts/fixed-bg-button-block.php'); ?>
 
 
 <?php
