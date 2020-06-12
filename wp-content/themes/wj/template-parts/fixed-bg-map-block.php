05cd019848f4494935c453cef92a6e6e60d267c6
Andy <aknapp1@gmail.com>
2020-06-12 14:05:15 -0400

    misc updates

--- wp-content/themes/wj/template-parts/fixed-bg-map-block.php
+++ wp-content/themes/wj/template-parts/fixed-bg-map-block.php
@@ -1,13 +1,11 @@
 <section class="fixed-bg-block big map">
     <div class="container">
-
         <iframe
           width="600"
           height="450"
           frameborder="0" style="border:0"
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAwzZ3vFrOY05-1LE_m7OK009tE6mfeeoE
-            &q=Space+Needle,Seattle+WA" allowfullscreen>
+            &q=915+Estes+Ct,+Schaumburg,+IL+60193/" allowfullscreen>
         </iframe>
-
     </div>
 </section>
